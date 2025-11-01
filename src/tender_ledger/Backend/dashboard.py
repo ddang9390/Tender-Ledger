a719843780def39624b4ec14acae0147884029fe
Daniel Dang <dnld1ng@yahoo.com>
2025-11-01 12:34:27 -0400

    Display basic pie charts on dashboard

---
+++ src/tender_ledger/Backend/dashboard.py
@@ -0,0 +1,52 @@
+# Author - Daniel Dang
+# Filename - dashboard_page.py
+# Purpose - Generates the charts for the dashboard
+
+from matplotlib.figure import Figure
+
+
+# Defining constants which are the indices of the elements that
+# appear in the expenses list
+CATEGORY_INDEX = 3
+PAYMENT_METHOD_INDEX = 2
+
+def generate_pie_charts(expenses):
+    """
+    Generates a pie chart for the dashboard page that shows
+    how the expenses are distributed by category and payment method
+
+    Argument:
+        expenses (List): List of the user's expenses
+
+    Returns:
+        category_pie: Pie chart representing distribution by categories
+        payment_method_pie: Pie chart representing distribution by payment methods
+    """
+    categories = {}
+    payment_methods = {}
+    
+    for expense in expenses:
+        category = expense[CATEGORY_INDEX]
+        if category not in categories.keys():
+            categories[category] = 0
+
+        payment_method = expense[PAYMENT_METHOD_INDEX]
+        if payment_method not in payment_methods.keys():
+            payment_methods[payment_method] = 0
+
+        categories[category] += 1
+        payment_methods[payment_method] += 1
+
+    # Making pie chart for distribution by categories
+    category_pie = Figure()
+    category_pie_axes = category_pie.add_subplot(111)
+    category_pie_axes.pie(categories.values(), labels = categories.keys())
+    category_pie_axes.set_title('Distribution by Categories')
+
+    # Making pie chart for distribution by payment methods
+    payment_method_pie = Figure()
+    payment_method_pie_axes = payment_method_pie.add_subplot(111)
+    payment_method_pie_axes.pie(payment_methods.values(), labels = payment_methods.keys())
+    payment_method_pie_axes.set_title('Distribution by Payment Methods')
+    
+    return category_pie, payment_method_pie
--- src/tender_ledger/GUI/Pages/dashboard_page.py
+++ src/tender_ledger/GUI/Pages/dashboard_page.py
@@ -1,8 +1,12 @@
 # Author - Daniel Dang
 # Filename - dashboard_page.py
-# Purpose - Handles the appearance of the expenses page
+# Purpose - Handles the appearance of the dashboard page
 
 import customtkinter
+from ...Backend.expenses import get_expenses_for_user
+from ...Backend.dashboard import generate_pie_charts
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class DashboardPage(customtkinter.CTkFrame):
     def __init__(self, parent, controller, db):
@@ -18,9 +22,37 @@ class DashboardPage(customtkinter.CTkFrame):
         self.controller = controller
         self.db = db
 
-        label = customtkinter.CTkLabel(self, text="Dashboard")
-        label.pack()
+        # TODO - change user id
+        self.user_id = -1
+        self.expenses = get_expenses_for_user(self.user_id, self.db)
 
+        # Header
+        label = customtkinter.CTkLabel(self, text="Dashboard")
+        label.grid(row=0, column=0)
+
+        # Filter Section
+        # TODO - make date range filters
+
+        # Tables
+        self.create_summary_section()
+
+    def create_summary_section(self):
+        """
+        Create summary section for dashboard
+        """
+        # Generate pie charts
+        category_pie, payment_method_pie = generate_pie_charts(self.expenses)
+        self.summary_frame = customtkinter.CTkFrame(self)
+
+        # Display category pie chart
+        category_pie_chart = FigureCanvasTkAgg(figure=category_pie, master=self.summary_frame)
+        category_pie_chart.get_tk_widget().grid(row=0, column=0)
+
+        # Display payment method pie chart
+        payment_method_pie_chart = FigureCanvasTkAgg(figure=payment_method_pie, master=self.summary_frame)
+        payment_method_pie_chart.get_tk_widget().grid(row=0, column=1)
+
+        self.summary_frame.grid(row=1, column=0)
 
 
     
